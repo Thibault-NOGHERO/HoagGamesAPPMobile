c06c50cb571d0fa301ab171f61f78c1e955428f9
Thibault NOGHERO <thibault.noghero@gmail.com>
2023-05-10 18:44:09 +0200

    ca marche

--- API/try/signup_process.php
+++ API/try/signup_process.php
@@ -1,5 +1,4 @@
 <?php
-// Vérifie si les données ont été envoyées via la méthode POST
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Récupère les données envoyées
     $data = json_decode(file_get_contents('php://input'), true);
@@ -8,75 +7,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $password = $data["password"] ?? "";
     $password_confirm = $data["password_confirm"] ?? "";
 
-    // Envoie une réponse JSON avec un message "ok"
+    // Initialise une variable pour stocker le message de réponse
+    $responseMessage = "";
+
+    // Votre code pour traiter les données ici...
+    // Par exemple :
+    if (!empty($pseudo) && !empty($email) && !empty($password) && $password === $password_confirm) {
+        // Le code pour insérer les données dans la base de données va ici...
+        // Si tout se passe bien, mettez à jour le message de réponse
+        $responseMessage = "User successfully created";
+    } else {
+        // S'il y a un problème, mettez à jour le message de réponse avec une description de l'erreur
+        $responseMessage = "Erreur : veuillez vérifier vos données.";
+    }
+
+    // Envoie une seule réponse JSON à la fin du script
     header('Content-Type: application/json');
-    echo json_encode(["message" => "okidoki"]);
-
-    header('Content-Type: application/json');
-    echo json_encode($pseudo);
+    echo json_encode(["message" => $responseMessage]);
 }
-session_start();
-require_once '../config.php';
-require_once '../db.php';
-if (!empty($pseudo) && !empty($email) && !empty($password) && $password === $password_confirm) {
-  $db = new Db();
-  $conn = $db->getConnection();
-
-  $stmt = $conn->prepare('SELECT * FROM users WHERE email = :email');
-  $stmt->bindParam(':email', $email);
-  $stmt->execute();
-  $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-  if (!$user) {
-      $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-      $stmt = $conn->prepare('INSERT INTO users (pseudo, email, password) VALUES (:pseudo, :email, :password)');
-      $stmt->bindParam(':pseudo', $pseudo);
-      $stmt->bindParam(':email', $email);
-      $stmt->bindParam(':password', $hashed_password);
-      $stmt->execute();
-
-      header('Content-Type: application/json');
-      echo json_encode(["message" => "User successfully created"]);
-      exit();
-  } else {
-      header('Content-Type: application/json');
-      echo json_encode(["message" => "L'email existe déjà. Veuillez choisir un autre email."]);
-  }
-} else {
-  header('Content-Type: application/json');
-  echo json_encode(["message" => "Veuillez remplir tous les champs et vérifier que les mots de passe correspondent."]);
-}
-
-  // Vérifie si toutes les données ont été envoyées
-  // if () {
-    
-    // Vérifie si les mots de passe correspondent
-    // if ($password === $password_confirm) {
-      
-      // TODO: Ajoutez le code ici pour enregistrer les données dans une base de données ou un autre endroit de stockage
-        
-      // Envoie une réponse JSON avec un message "ok"
-      // header('Content-Type: application/json');
-      // echo json_encode(["message" => "okidoki"]);
-      
-    // } else {
-      // Les mots de passe ne correspondent pas
-      // Envoie une réponse JSON avec un message "error"
-    //   header('Content-Type: application/json');
-    //   echo json_encode(["message" => "error", "error_message" => "Les mots de passe ne correspondent pas."]);
-    // }
-    
-  // } else {
-    // Toutes les données n'ont pas été envoyées
-    // Envoie une réponse JSON avec un message "error"
-  //   header('Content-Type: application/json');
-  //   echo json_encode(["message" => "error", "error_message" => "Toutes les données n'ont pas été envoyées."]);
-  // }
-  
-// } else {
-  // La requête n'est pas une requête POST
-  // Envoie une réponse JSON avec un message "error"
-//   header('Content-Type: application/json');
-//   echo json_encode(["message" => "error", "error_message" => "La requête n'est pas une requête POST."]);
-// }
+?>
