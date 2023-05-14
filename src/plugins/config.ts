290c23c60abbbcf311dfd8a63de9b2299d411095
Thibault NOGHERO <thibault.noghero@gmail.com>
2023-05-14 22:35:59 +0200

    test

---
+++ src/plugins/config.ts
@@ -0,0 +1,13 @@
+interface Config {
+  publicPath: string;
+  baseUrl: string;
+  apiUrl: string;
+}
+
+const config: Config = {
+  publicPath: "testHoaggamesApp/",
+  baseUrl: "http://hoaggamesapp.epizy.com",
+  apiUrl: "testHoaggamesApp/API/try",
+};
+
+export default config;
--- src/user/Inscription/TheInscriptionView.vue
+++ src/user/Inscription/TheInscriptionView.vue
@@ -1,38 +1,47 @@
 <template>
   <div id="signup-form">
     <form @submit.prevent="signupProcess">
-      <div class="hg-TheInscriptionView-element" @click="focusInput">
-        <div class="hg-TheInscriptionView-input">
-          <label for="pseudo">Pseudo : </label>
-          <input
-            type="text"
-            v-model="formData.pseudo"
-            id="pseudo"
-            ref="inputRef"
-            required
-          />
-        </div>
+      <div class="hg-TheInscriptionView-element">
+        <label for="pseudo">Pseudo : </label>
+
+        <input
+          type="text"
+          v-model="formData.pseudo"
+          id="pseudo"
+          ref="inputRef"
+          required
+          class="hg-TheInscriptionView-input"
+        />
       </div>
-      <div class="hg-TheInscriptionView-input">
+
+      <div class="hg-TheInscriptionView-element">
         <label for="email">Email:</label>
-        <input type="email" v-model="formData.email" id="email" required />
+        <input
+          type="email"
+          v-model="formData.email"
+          id="email"
+          required
+          class="hg-TheInscriptionView-input"
+        />
       </div>
-      <div class="hg-TheInscriptionView-input">
+      <div class="hg-TheInscriptionView-element">
         <label for="password">Mot de passe:</label>
         <input
           type="password"
           v-model="formData.password"
           id="password"
           required
+          class="hg-TheInscriptionView-input"
         />
       </div>
-      <div class="hg-TheInscriptionView-input">
+      <div class="hg-TheInscriptionView-element">
         <label for="password_confirm">Confirmer le mot de passe:</label>
         <input
           type="password"
           v-model="formData.password_confirm"
           id="password_confirm"
           required
+          class="hg-TheInscriptionView-input"
         />
       </div>
       <div class="">
@@ -46,8 +55,9 @@
 
 <script setup lang="ts">
 import axios from "axios";
-import { ref, computed, onMounted } from "vue";
+import { ref, computed } from "vue";
 import { Router, useRouter } from "vue-router";
+import config from "@/plugins/config";
 
 const formData = ref({
   pseudo: "",
@@ -60,7 +70,8 @@ const router: Router = useRouter();
 
 const signupProcess = async () => {
   try {
-    const apiUrl = `${process.env.VUE_APP_BASE_URL}/${process.env.VUE_APP_URL_API}/signup_process.php`;
+    // const apiUrl = `${process.env.VUE_APP_BASE_URL}/${process.env.VUE_APP_URL_API}/signup_process.php`;
+    const apiUrl = `${config.baseUrl}/${config.apiUrl}/signup_process.php`;
     const data = {
       pseudo: formData.value.pseudo,
       email: formData.value.email,
@@ -77,25 +88,6 @@ const signupProcess = async () => {
   }
 };
 
-// const signupProcess = async () => {
-//   try {
-//     const apiUrl = `${process.env.VUE_APP_BASE_URL}/${process.env.VUE_APP_URL_API}/test_connexion.php`;
-//     const data = {
-//       pseudo: formData.value.pseudo,
-//       email: formData.value.email,
-//       password: formData.value.password,
-//       password_confirm: formData.value.password_confirm,
-//       // bio: "Je suis un nouvel utilisateur !", // Valeur par défaut pour bio
-//       // plateform: "Aucune plateforme ajoutée pour le moment.", // Valeur par défaut pour plateform
-//     };
-//     const response = await axios.post(apiUrl, data);
-//     console.log(response);
-//     router.push("/connexion");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
 const formIsValid = computed(() => {
   return (
     formData.value.pseudo &&
@@ -104,18 +96,6 @@ const formIsValid = computed(() => {
     formData.value.password_confirm &&
     formData.value.password === formData.value.password_confirm
   );
-});
-
-const inputRef = ref(null);
-
-const focusInput = () => {
-  if (inputRef.value) {
-    (inputRef.value as HTMLInputElement).focus();
-  }
-};
-
-onMounted(() => {
-  focusInput();
 });
 </script>
 
